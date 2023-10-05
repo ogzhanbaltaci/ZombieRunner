55ca5b4744fa523ec5119b2b214fabfe0aed4bba
Oguzhan Baltaci <ogzhan.baltaci@gmail.com>
2023-10-05 16:24:24 +0300

    Added some particle effects for gun and animation for enemy

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -44,11 +44,14 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", true);
         Debug.Log(name + " has seeked and is destroyed " + target.name);
     }
 
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 
